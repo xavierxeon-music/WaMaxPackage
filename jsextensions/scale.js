14258909eda1950763d0012cd723541677c7e4d9
Ralf Waspe <raw@mmmi.sdu.dk>
2023-03-31 12:34:16 +0200

    more scale things

--- jsextensions/scale.js
+++ jsextensions/scale.js
@@ -2,6 +2,7 @@
 
 function Scale() {
 
+   // default is s major
    this.notes = {
       0: true, // c
       1: false, // c#
@@ -15,11 +16,52 @@ function Scale() {
       9: true, // a
       10: false // a#
    };
+
+   const names = {
+      "c": 0,
+      "c#": 1,
+      "d": 2,
+      "d#": 3,
+      "e": 4,
+      "f": 5,
+      "f#": 6,
+      "g": 7,
+      "g#": 8,
+      "a": 9,
+      "a#": 10
+   }
+
    return this;
 }
 
-Scale.prototype.setPredifined = function (name) {
-   // read dict and set notes
+Scale.prototype.clear() = function () {
+   for (var index = 0; index < 11; index++)
+      notes[index] = false;
+}
+
+Scale.prototype.setPredifined = function (baseNote, major = true) {
+
+   clear();
+
+   if (false === baseNote in names)
+      return;
+
+   var startIndex = names[baseNote];
+
+   if (major) {
+      const majorNotes = [0, 2, 4, 5, 7, 9];
+      for (var offset in majorNotes) {
+         var index = (startIndex + offset) % 11;
+         notes[index] = true;
+      }
+   }
+   else {
+      const minorNotes = [0, 2, 4, 6, 7, 9];
+      for (var offset in minorNotes) {
+         var index = (startIndex + offset) % 11;
+         notes[index] = true;
+      }
+   }
 }
 
 Scale.prototype.closestMatch = function (midiNote) {
