361a44e3b9c9cdda3bee8e53a0086e1570b4cce2
Ralf Waspe <rwaspe@icloud.com>
2023-09-17 00:57:46 +0200

    improrved taskmaster

--- javascript/seqeuncer/taskmaster.js
+++ javascript/seqeuncer/taskmaster.js
@@ -8,28 +8,40 @@ outlets = 1;
 setoutletassist(0, "event");
 
 var scheduleFile = null;
+var schedule = {};
 
 function create() {
 
-   print("create");
+   dummy = { "1.1": ["A", "B"] };
+   saveJsonFile(dummy, scheduleFile);
+   // print("create", scheduleFile);
 }
 
 function read(fileName) {
 
    scheduleFile = fileName;
-   print("read", fileName);
    reload();
 }
 
 function time(bars, beats) {
 
-   print("time", bars, beats);
+   var key = bars + "." + beats;
+   eventList = schedule[key]
+   if (undefined == eventList)
+      return;
+
+   for (index in eventList) {
+
+      var event = eventList[index];
+      outlet(0, event);
+   }
 }
 
 function reload() {
 
-   print("reload", scheduleFile);
    if (!scheduleFile)
       return;
+
+   schedule = readJsonFile(scheduleFile);
 }
 
