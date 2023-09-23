616655ad31140827b6bce81da0b0efb1175a9ff2
Ralf Waspe <rwaspe@icloud.com>
2023-09-23 13:06:56 +0200

    nicer singleton

--- misc/sequencer/nosferatu/mainwidget.py
+++ misc/sequencer/nosferatu/mainwidget.py
@@ -4,22 +4,27 @@ import sys
 
 from PySide6.QtCore import QSettings
 from PySide6.QtWidgets import QTextEdit
+from PySide6.QtNetwork import QLocalServer, QLocalSocket
 
 
 class MainWidget(QMainWindow):
 
-    def __init__(self, fileName):
+    def __init__(self):
 
         super().__init__()
+        self._server = None
 
         self.rawView = QTextEdit('Hello')
         self.setCentralWidget(self.rawView)
 
-        self.load(fileName)
-
         qtsettings = QSettings()
         self.restoreGeometry(qtsettings.value('geometry'))
         self.restoreState(qtsettings.value('state'))
+
+    def __del__(self):
+
+        if self._server:
+            self._server.close()
 
     def closeEvent(self, event):
 
@@ -29,9 +34,33 @@ class MainWidget(QMainWindow):
 
         super().closeEvent(event)
 
+    def singletonLoad(self, fileName):
+
+        _socketName = "nosferatu_editor"
+
+        def serverConnected():
+            socket = self._server.nextPendingConnection()
+            socket.waitForReadyRead()
+            fileName = bytes(socket.readAll()).decode()
+            self.load(fileName)
+
+        socket = QLocalSocket()
+        socket.connectToServer(_socketName)
+        if socket.waitForConnected():  # found server
+            socket.write(fileName.encode())
+            socket.waitForBytesWritten()
+            return False
+        elif not self._server:
+            self._server = QLocalServer()
+            self._server.newConnection.connect(serverConnected)
+            self._server.listen(_socketName)
+
+        self.load(fileName)
+        return True
+
     def load(self, fileName):
 
-        self.setWindowTitle(f'Nosferatu [{fileName}]')
+        self.setWindowTitle(f'Nosferatu Editor [{fileName}]')
 
         with open(fileName, 'r') as infile:
             text = infile.read()
--- misc/sequencer/nosferatu_ui.py
+++ misc/sequencer/nosferatu_ui.py
@@ -5,7 +5,6 @@ import sys
 
 from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QApplication
-from PySide6.QtNetwork import QLocalServer, QLocalSocket
 
 from nosferatu import MainWidget
 
@@ -28,38 +27,19 @@ def main():
     timer.start(500)
     timer.timeout.connect(lambda: None)  # Let the interpreter run each 500 ms.
 
+    mainWindow = MainWidget()
+
     fileName = ' '.join(sys.argv[1:])
-    index = fileName.index(':')
+    index = fileName.find(':')
     if index >= 0:
         fileName = fileName[index+1:]
 
-    socketName = 'nosfertu_editor'
-
-    socket = QLocalSocket()
-    socket.connectToServer(socketName)
-    if socket.waitForConnected():  # found server
-        socket.write(fileName.encode())
-        socket.waitForBytesWritten()
+    if not mainWindow.singletonLoad(fileName):  # other instance of application is running
+        # print("open file in exisiting application")
         return 0
 
-    mainWindow = MainWidget(fileName)
-
-    server = QLocalServer()
-
-    def serverConnected():
-        socket = server.nextPendingConnection()
-        socket.waitForReadyRead()
-        fileName = bytes(socket.readAll()).decode()
-        mainWindow.load(fileName)
-
-    server.newConnection.connect(serverConnected)
-    server.listen(socketName)
-
     mainWindow.show()
-
     status = app.exec()
-    server.close()
-
     sys.exit(status)
 
 
