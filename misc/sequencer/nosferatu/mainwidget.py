472941cc423effda06acf7b3e74737f9b62ac8ce
Ralf Waspe <rwaspe@icloud.com>
2023-09-24 01:13:22 +0200

    added files

--- misc/sequencer/nosferatu/mainwidget.py
+++ misc/sequencer/nosferatu/mainwidget.py
@@ -38,12 +38,6 @@ class MainWidget(QMainWindow):
 
         _socketName = "nosferatu_editor"
 
-        def serverConnected():
-            socket = self._server.nextPendingConnection()
-            socket.waitForReadyRead()
-            fileName = bytes(socket.readAll()).decode()
-            self.load(fileName)
-
         socket = QLocalSocket()
         socket.connectToServer(_socketName)
         if socket.waitForConnected():  # found server
@@ -52,11 +46,17 @@ class MainWidget(QMainWindow):
             return False
         elif not self._server:
             self._server = QLocalServer()
-            self._server.newConnection.connect(serverConnected)
+            self._server.newConnection.connect(self._serverConnected)
             self._server.listen(_socketName)
 
         self.load(fileName)
         return True
+
+    def _serverConnected(self):
+        socket = self._server.nextPendingConnection()
+        socket.waitForReadyRead()
+        fileName = bytes(socket.readAll()).decode()
+        self.load(fileName)
 
     def load(self, fileName):
 
--- misc/sequencer/nosferatu_ui.py
+++ misc/sequencer/nosferatu_ui.py
@@ -22,21 +22,20 @@ def main():
     app.setOrganizationDomain('schweinesystem.eu')
     app.setApplicationName('Nosferatu Editor Tool')
 
-    signal.signal(signal.SIGINT, signit_handler)
-    timer = QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)  # Let the interpreter run each 500 ms.
-
-    mainWindow = MainWidget()
-
     fileName = ' '.join(sys.argv[1:])
     index = fileName.find(':')
     if index >= 0:
         fileName = fileName[index+1:]
 
+    mainWindow = MainWidget()
     if not mainWindow.singletonLoad(fileName):  # other instance of application is running
         # print("open file in exisiting application")
         return 0
+
+    signal.signal(signal.SIGINT, signit_handler)
+    timer = QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)  # Let the interpreter run each 500 ms.
 
     mainWindow.show()
     status = app.exec()
