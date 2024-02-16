f362b27529b13982a915300155c467cf569f850d
Ralf Waspe <rwaspe@icloud.com>
2024-02-16 17:23:22 +0100

    max gui for small rack

--- media/maxgui.js
+++ media/maxgui.js
@@ -52,22 +52,124 @@ function bodyStyle(name, value) {
    document.body.style[name] = value;
 }
 
+// header
+function createTitle(text, docWidth, leftMargin, topMargin) {
+
+   if (docWidth)
+      document.body.style["width"] = docWidth.toString() + "px";
+   if (leftMargin)
+      document.body.style.marginLeft = leftMargin.toString() + "px";
+   if (topMargin)
+      document.body.style.marginTop = topMargin.toString() + "px";
+
+   const title = document.createElement("div");
+   title.className = "title";
+   title.innerText = text;
+   document.body.appendChild(title);
+}
+
 // tabs
-function showTab(evt, tabName) {
 
-   // hide all tabs
-   let tabcontent = document.getElementsByClassName("tabcontent");
-   for (let i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+class TabBar {
+   constructor(defaultLayout) {
+
+      this.layouttext = TabBar.#extractLayout(defaultLayout);
+
+      this.element = document.createElement("div");
+      this.element.setAttribute("class", "tabbar");
+      document.body.appendChild(this.element);
    }
 
-   // deactivae all tab buttons
-   let tabbutton = document.getElementsByClassName("tabbutton");
-   for (let i = 0; i < tabbutton.length; i++) {
-      tabbutton[i].className = tabbutton[i].className.replace(" active", "");
+   addTab(title, defaultOpen, localLayout) {
+
+      let id = btoa(title);
+
+      let tabbutton = document.createElement("button");
+      tabbutton.setAttribute("class", "tabbutton");
+      tabbutton.addEventListener("click", (evt) => { this.#showTab(evt, id); });
+      //tabbutton.setAttribute("onclick", "TabBar.showTab(event, '" + id + "')");
+      tabbutton.innerText = title;
+      this.element.appendChild(tabbutton);
+
+      let tabcontent = document.createElement("div");
+      tabcontent.setAttribute("class", "tabcontent");
+      tabcontent.setAttribute("id", id);
+      document.body.appendChild(tabcontent);
+
+      if (localLayout) {
+         let locallayouttext = TabBar.#extractLayout(localLayout);
+         tabcontent.style.gridTemplateColumns = locallayouttext;
+      }
+      else if (this.layouttext) {
+         tabcontent.style.gridTemplateColumns = this.layouttext;
+      }
+
+      if (defaultOpen) {
+         tabbutton.className += " active";
+         tabcontent.style.display = "grid";
+      }
+      else {
+         tabcontent.style.display = "none";
+      }
+
+      return tabcontent;
    }
 
-   // show current tab and active corresponfing button
-   document.getElementById(tabName).style.display = "grid";
-   evt.currentTarget.className += " active";
+   #showTab(evt, tabName) {
+
+      // hide all tabs
+      let tabcontent = document.getElementsByClassName("tabcontent");
+      for (let i = 0; i < tabcontent.length; i++) {
+         tabcontent[i].style.display = "none";
+      }
+
+      // deactivae all tab buttons
+      let tabbutton = document.getElementsByClassName("tabbutton");
+      for (let i = 0; i < tabbutton.length; i++) {
+         tabbutton[i].className = tabbutton[i].className.replace(" active", "");
+      }
+
+      // show current tab and active corresponfing button
+      document.getElementById(tabName).style.display = "grid";
+      evt.currentTarget.className += " active";
+   }
+
+   static #extractLayout(layout) {
+
+      if (!layout)
+         return undefined;
+
+      let text = "";
+      for (let index in layout) {
+         if (0 != text.length)
+            text += " ";
+
+         var value = layout[index];
+         if ("number" == typeof value)
+            value = value.toString() + "px";
+
+         text += value;
+      }
+      return text;
+   }
+}
+
+// div
+function createContent(parent, value, alignment, id) {
+
+   let content = document.createElement("div");
+   if (id) {
+      let span = document.createElement("span");
+      span.setAttribute("id", id);
+      span.innerHTML = value.toString();
+      content.appendChild(span);
+   }
+   else {
+      content.innerHTML = value.toString();
+   }
+
+   content.style.textAlign = alignment;
+   parent.appendChild(content);
+
+   return content;
 }
--- media/mixer/rack_small_ui.js
+++ media/mixer/rack_small_ui.js
@@ -1,3 +1,73 @@
-const para = document.createElement("p");
-para.innerText = "This is a paragraph in a file";
-document.body.appendChild(para);
+createTitle("small rack", 180, 0, 0);
+
+let tabBar = new TabBar([70, 30, 70]);
+let tabA = tabBar.addTab("ES8", true);
+let tabB = tabBar.addTab("Optix");
+
+let portDict = {};
+
+const es8Ports = [1, 2, 3, 4, 5, 6, 7, 8];
+for (let index in es8Ports) {
+
+   let sendKey = "se" + index.toString();
+   let sendPort = createContent(tabA, "&#9675;", "right", sendKey);
+   portDict[sendKey] = sendPort;
+
+   let center = createContent(tabA, es8Ports[index], "center");
+   center.style.fontWeight = "bold";
+
+   if (index < 4) {
+      let receiveKey = "re" + index.toString();
+      let receivePort = createContent(tabA, "&#9675;", "left", receiveKey);
+      portDict[receiveKey] = receivePort;
+   }
+   else
+      createContent(tabA, "", "left");
+
+}
+
+const optixPorts = [1, 2, 3, 4, 5, 6, 7, 8];
+for (let index in es8Ports) {
+   let sendKey = "so" + index.toString();
+   let sendPort = createContent(tabB, "&#9675;", "right", sendKey);
+   portDict[sendKey] = sendPort;
+
+   let center = createContent(tabB, es8Ports[index], "center");
+   center.style.fontWeight = "bold";
+
+   let receiveKey = "ro" + index.toString();
+   let receivePort = createContent(tabB, "&#9675;", "left", receiveKey);
+   portDict[receiveKey] = receivePort;
+}
+
+function blank() {
+
+   let portLabels = document.getElementsByClassName("port_name");
+   for (let i = 0; i < portLabels.length; i++) {
+      portLabels[i].innerHTML = "&#9675;";
+   }
+}
+
+max.bindInlet('loadDict', loadDict);
+function loadDict() {
+
+   blank();
+
+   max.getDict("smallRackPorts", function (maxDict) {
+
+      for (key in maxDict) {
+
+         let output = portDict[key];
+         if (!output)
+            continue;
+
+         let text = maxDict[key];
+         if (key[0] == "s")
+            text = text + " &#10132;";
+         else
+            text = "&#10132; " + text;
+         output.innerHTML = text;
+      }
+   });
+}
+
