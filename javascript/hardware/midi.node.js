df25f8334fc07bce3837db40780a0be23fd84284
Ralf Waspe <rwaspe@icloud.com>
2024-03-04 21:05:34 +0100

    started node midi

--- javascript/hardware/midi.node.js
+++ javascript/hardware/midi.node.js
@@ -1,13 +1,37 @@
-autowatch = 1;
+const maxAPI = require('max-api');
+const midi = require("midi");
 
-// inlets and outlets
-inlets = 1;
-setinletassist(0, "text");
 
-outlets = 1;
-setoutletassist(0, "text");
+const output = new midi.Output();
 
-function bang(){
-   post("bang");
-}
+const input = new midi.Input();
+input.on('message', (deltaTime, message) => {
 
+   console.log(message);
+   maxAPI.outlet("midi", message);
+});
+
+
+maxAPI.addHandler('open', (deviceName) => {
+
+   for (let index = 0; index < output.getPortCount(); index++) {
+      let name = output.getPortName(index);
+      if (name != deviceName)
+         continue;
+      input.openPort(index);
+      break;
+   }
+
+   for (let index = 0; index < input.getPortCount(); index++) {
+      let name = input.getPortName(index);
+      if (name != deviceName)
+         continue;
+      output.openPort(index);
+      break;
+   }
+});
+
+
+maxAPI.addHandler('send', (...args) => {
+   output.sendMessage([176, 22, 1]);
+});
