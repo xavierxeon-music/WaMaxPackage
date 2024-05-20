559032e622719d770eb977e625ea727e0469e0fc
Ralf Waspe <rwaspe@icloud.com>
2024-05-20 02:38:31 +0200

    mixer stuff

--- media/mixer/mixer_console_ui.js
+++ media/mixer/mixer_console_ui.js
@@ -7,3 +7,19 @@ tableDiv.inlineHTML("./mixer/_mixer_layout.html");
 
 let masterFader = document.getElementById("master_fader");
 new MixerFader(masterFader);
+
+let channelFader = document.getElementById("channel_faders");
+for (var index = 0; index < 10; index++) {
+   var div = new Div(channelFader);
+   div.makeHorizontal();
+   new MixerFader(div.element);
+}
+
+let returnFader = document.getElementById("return_faders");
+for (var index = 0; index < 2; index++) {
+   var div = new Div(returnFader);
+   div.makeHorizontal();
+   new MixerFader(div.element);
+}
+
+
