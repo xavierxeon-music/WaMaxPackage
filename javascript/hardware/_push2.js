5200204d4736e892966d1535e977ca6bf293058e
Ralf Waspe <rwaspe@icloud.com>
2024-07-12 09:35:57 +0200

    push2 refactoring

--- javascript/hardware/_push2.js
+++ javascript/hardware/_push2.js
@@ -1,1 +1,132 @@
 //
+
+function Push2Device() {
+
+   this.internal = new Global("Push2");
+
+   this.encoderIdList = [14, 15, 71, 72, 73, 74, 75, 76, 77, 78, 79];
+
+   this.colorIndexBuffer = {};
+   this.colorWhiteIndex = 0;
+
+   this.whiteIndexBuffer = {};
+
+   var data = readJsonFile("push2_data.json");
+   this.colorList = data["colors"];
+   this.whiteList = data["whites"];
+   this.padmap = data["pads"];
+   this.buttonmap = data["buttons"];
+
+   var white = new Color("ffffff");
+   this.colorWhiteIndex = this._findNearestMatchInColorList(white);
+   this.colorIndexBuffer[white.hex] = this.colorWhiteIndex;
+
+   for (var index = 0; index < this.colorList.length; index += 1) {
+
+      var color = this.colorList[index];
+      this.colorIndexBuffer[color] = index;
+   }
+
+   this.whiteIndexBuffer["000000"] = 0;
+   this.whiteIndexBuffer["ffffff"] = 127;
+
+   return this;
+}
+
+Push2Device.prototype.padId = function (name) {
+
+   if (name in this.padmap)
+      return this.padmap[name];
+
+   return 36; // fallback to pad on bottom left   
+}
+
+Push2Device.prototype.padName = function (id) {
+
+   for (var name in this.padmap) {
+      if (id === this.padmap[name])
+         return name;
+   }
+
+   return undefined;
+}
+
+Push2Device.prototype.buttonId = function (name) {
+
+   if (name in this.buttonmap)
+      return this.buttonmap[name][0];
+
+   return 85; // fallback to play button on bottom left   
+}
+
+Push2Device.prototype.buttonName = function (id) {
+
+   for (var name in this.buttonmap) {
+      if (id === this.buttonmap[name][0])
+         return name;
+   }
+
+   return undefined;
+}
+
+Push2Device.prototype.buttonIsColor = function (name) {
+
+   if (name in this.buttonmap)
+      return (1 == this.buttonmap[name][1]);
+
+   return 1; // fallback to play button on bottom left   
+}
+
+Push2Device.prototype.colorSelect = function (isColor, inColor) {
+
+   var color = new Color(inColor);
+   if (0 === isColor)
+      return this._findNearestMatchInWhiteList(color);
+
+   return this._findNearestMatchInColorList(color);
+}
+
+Push2Device.prototype._findNearestMatchInColorList = function (color) {
+
+   if (color.hex in this.colorIndexBuffer)
+      return this.colorIndexBuffer[color.hex];
+
+   var color_index = this.colorWhiteIndex;
+   var minDistance = 0;
+   for (var index = 0; index < this.colorList.length; index++) {
+
+      const test = this.colorList[index];
+      const rgb = new Color(test);
+
+      const distance = color.distance(rgb);
+
+      if (0 === index || distance < minDistance) {
+         minDistance = distance;
+         color_index = index;
+      }
+   }
+
+   this.colorIndexBuffer[color.hex] = color_index;
+   return color_index;
+}
+
+Push2Device.prototype._findNearestMatchInWhiteList = function (color) {
+
+   if (color.hex in this.whiteIndexBuffer)
+      return this.whiteIndexBuffer[color.hex];
+
+   const luma = color.luma();
+
+   var white__index = 127;
+   for (var index = 1; index < whiteList.length; index++) {
+      const last = this.whiteList[index - 1];
+      const current = this.whiteList[index];
+      if (luma > last && luma <= current) {
+         white__index = index;
+         break;
+      }
+   }
+
+   this.whiteIndexBuffer[color.hex] = white__index;
+   return white__index;
+}
--- javascript/hardware/push2_device.js
+++ javascript/hardware/push2_device.js
@@ -9,19 +9,8 @@ setinletassist(0, "lookup");
 outlets = 1;
 setoutletassist(0, "id");
 
-var data = readJsonFile("push2_data.json");
-var encoderIdList = [14, 15, 71, 72, 73, 74, 75, 76, 77, 78, 79];
-
-var colorList = undefined;
-var colorIndexBuffer = {};
-var colorWhiteIndex = 0;
-
-var whiteList = undefined;
-var whiteIndexBuffer = {};
-
-function loadbang() {
-   init();
-}
+include("_push2.js");
+var device = new Push2Device();
 
 // midi routing
 function midiPad(id, value) {
@@ -34,7 +23,7 @@ function midiPad(id, value) {
 
 function midiButton(id, value) {
 
-   var isEncoder = (encoderIdList.indexOf(id) > -1);
+   var isEncoder = (device.encoderIdList.indexOf(id) > -1);
    if (isEncoder) {
       if (1 == value)
          messnamed("push2_encoder_down", id);
@@ -56,11 +45,7 @@ function midiSlider(value) {
 
 function padClicked(name, clicked) {
 
-   var id = 36; // fallback to pad on bottom left   
-
-   var padmap = data["pads"];
-   if (name in padmap)
-      id = padmap[name];
+   var id = device.padId(name);
 
    if (clicked)
       midiPad(id, 127);
@@ -71,11 +56,7 @@ function padClicked(name, clicked) {
 
 function buttonClicked(name, clicked) {
 
-   var id = 85; // fallback to play button on bottom left   
-
-   var buttonmap = data["buttons"];
-   if (name in buttonmap)
-      id = buttonmap[name][0];
+   var id = device.buttonId(name);
 
    if (clicked)
       midiButton(id, 127);
@@ -86,53 +67,34 @@ function buttonClicked(name, clicked) {
 // name conversion
 function push2Pad(id) {
 
-   var padmap = data["pads"];
-   for (var key in padmap) {
-      const value = padmap[key];
-      if (id === value) {
-         outlet(0, ["pad", key]);
-         return;
-      }
-   }
+   var name = device.padName(id);
+   if (name)
+      outlet(0, ["pad", name]);
 }
 
 function pad2Push(name) {
 
-   var id = 36; // fallback to pad on bottom left   
-
-   var padmap = data["pads"];
-   if (name in padmap)
-      id = padmap[name];
-
+   var id = device.padId(name);
    outlet(0, id);
 }
 
 function push2Button(id) {
 
-   var buttonmap = data["buttons"];
-   for (var name in buttonmap) {
-      const value = buttonmap[name][0];
-      const isColor = (1 == buttonmap[name][1]);
-      if (value === id) {
-         if (isColor)
-            outlet(0, ["color_button", name]);
-         else
-            outlet(0, ["button", name]);
-         return;
-      }
-   }
+   var name = device.buttonName(id);
+   if (!name)
+      return;
+
+   var isColor = device.buttonIsColor(name);
+   if (isColor)
+      outlet(0, ["color_button", name]);
+   else
+      outlet(0, ["button", name]);
 }
 
 function button2Push(name) {
 
-   var id = 85; // fallback to play button on bottom left   
-   var isColor = true;
-
-   var buttonmap = data["buttons"];
-   if (name in buttonmap) {
-      id = buttonmap[name][0];
-      isColor = (1 == buttonmap[name][1]);
-   }
+   var id = device.buttonId(name);
+   var isColor = device.buttonIsColor(name);
 
    outlet(0, [id, isColor]);
 }
@@ -140,132 +102,33 @@ function button2Push(name) {
 // color functions
 function padColor(isColor, id, inColor) {
 
-   var color_index = colorSelect(isColor, inColor);
+   var color_index = device.colorSelect(isColor, inColor);
    outlet(0, ["pad", id, color_index]);
 }
 
 function padDummyColor(isColor, id, inColor) {
 
-   var color_index = colorSelect(isColor, inColor);
-   var newColor = colorList[color_index];
+   var color_index = device.colorSelect(isColor, inColor);
+   var newColor = device.colorList[color_index];
 
-   var padmap = data["pads"];
-   for (var key in padmap) {
-      const value = padmap[key];
-      if (id === value) {
-         outlet(0, ["setColor", key, newColor]);
-         return;
-      }
-   }
+   var name = device.padName(id);
+   if (name)
+      outlet(0, ["setColor", name, newColor]);
 }
 
 function buttonColor(isColor, id, inColor) {
 
-   var color_index = colorSelect(isColor, inColor);
+   var color_index = device.colorSelect(isColor, inColor);
    outlet(0, ["button", id, color_index]);
 }
 
 function buttonDummyColor(isColor, id, inColor) {
 
-   var color_index = colorSelect(isColor, inColor);
-   var newColor = colorList[color_index];
+   var color_index = device.colorSelect(isColor, inColor);
+   var newColor = device.colorList[color_index];
 
-   var buttonmap = data["buttons"];
-   for (var name in buttonmap) {
-      const value = buttonmap[name][0];
-      if (value === id) {
-         outlet(0, ["setColor", name, newColor]);
-         return;
-      }
-   }
+   var name = device.buttonName(id);
+   if (name)
+      outlet(0, ["setColor", name, newColor]);
 
 }
-
-colorSelect.local = 1;
-function colorSelect(isColor, inColor) {
-
-   var color = new Color(inColor);
-   var color_index = (1 === isColor) ? findNearestMatchInColorList(color) : findNearestMatchInWhiteList(color);
-
-   return color_index;
-}
-
-
-findNearestMatchInColorList.local = 1;
-function findNearestMatchInColorList(color) {
-
-   if (colorList === undefined) // need colorList to work with
-      init();
-
-   if (color.hex in colorIndexBuffer)
-      return colorIndexBuffer[color.hex];
-
-   var color_index = colorWhiteIndex;
-   var minDistance = 0;
-   for (var index = 0; index < colorList.length; index++) {
-
-      const test = colorList[index];
-      const rgb = new Color(test);
-
-      const distance = color.distance(rgb);
-
-      if (0 === index || distance < minDistance) {
-         minDistance = distance;
-         color_index = index;
-      }
-   }
-
-   //post("MATCH", color , color_index, "\n");
-
-   colorIndexBuffer[color.hex] = color_index;
-   return color_index;
-}
-
-findNearestMatchInWhiteList.local = 1;
-function findNearestMatchInWhiteList(color) {
-
-   if (whiteList === undefined) // need whiteList to work with
-      init();
-
-   if (color.hex in whiteIndexBuffer)
-      return whiteIndexBuffer[color.hex];
-
-   const luma = color.luma();
-
-   var white__index = 127;
-   for (var index = 1; index < whiteList.length; index++) {
-      const last = whiteList[index - 1];
-      const current = whiteList[index];
-      if (luma > last && luma <= current) {
-         white__index = index;
-         break;
-      }
-   }
-
-   //post(color, luma,  white__index, "\n");
-   whiteIndexBuffer[color.hex] = white__index;
-   return white__index;
-}
-
-// internal
-init.local = 1;
-function init() {
-
-   colorList = data["colors"];
-   whiteList = data["whites"];
-
-   var white = new Color("ffffff");
-   colorWhiteIndex = findNearestMatchInColorList(white);
-   colorIndexBuffer[white.hex] = colorWhiteIndex;
-
-   for (var index = 0; index < colorList.length; index += 1) {
-
-      var color = colorList[index];
-      colorIndexBuffer[color] = index;
-   }
-
-   whiteIndexBuffer["000000"] = 0;
-   whiteIndexBuffer["ffffff"] = 127;
-}
-
-
