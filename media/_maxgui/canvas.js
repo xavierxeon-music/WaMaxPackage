5eb7f4faad4efd3cea5b51574de0b6e6d9e0901c
Ralf Waspe <rwaspe@icloud.com>
2024-08-01 19:07:11 +0200

    started canvas functions

--- media/_maxgui/canvas.js
+++ media/_maxgui/canvas.js
@@ -21,6 +21,47 @@ class Canvas extends BaseElement {
          height = height.toString() + "px";
 
       this.element.setAttribute("height", height);
-
    }
+
+   clear() {
+      this.ctx.clearRect(0, 0, this.element.width, this.element.height);
+   }
+
+   circle(x, y, radius, color, length, offset) {
+
+      if (undefined == length)
+         length = 1.0;
+      if (undefined == offset)
+         offset = 0;
+
+      let startAngle = offset;
+      let endAngle = offset + (length * 2 * Math.PI);
+
+      this.ctx.fillStyle = color;
+
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, radius, startAngle, endAngle);
+      this.ctx.fill();
+      this.ctx.closePath();
+   }
+
+   ring(x, y, radius, width, color, length, offset) {
+
+      if (undefined == length)
+         length = 1.0;
+      if (undefined == offset)
+         offset = 0;
+
+      let startAngle = offset;
+      let endAngle = offset + (length * 2 * Math.PI);
+
+      this.ctx.strokeStyle = color;
+
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, radius, startAngle, endAngle);
+      this.ctx.lineWidth = width;
+      this.ctx.stroke();
+      this.ctx.closePath();
+   }
+
 }
--- media/hardware/grid_pot16_ui.js
+++ media/hardware/grid_pot16_ui.js
@@ -46,9 +46,7 @@ class GridCanvas extends Canvas {
 
    update() {
 
-      this.ctx.clearRect(0, 0, this.element.width, this.element.height);
-
-      const zeroAngle = 0.5 * Math.PI;
+      this.clear();
 
       var outlineColor = "#666666";
       var fullColor = "#ffffff";
@@ -62,57 +60,36 @@ class GridCanvas extends Canvas {
             let index = ((4 - col) * 10) + (row + 1)
 
             // outer circle
-
-            this.ctx.fillStyle = outlineColor;
-
-            this.ctx.beginPath();
-            this.ctx.arc(x, y, 14, 0, 2 * Math.PI);
-            this.ctx.lineWidth = 6;
-            this.ctx.fill();
-            this.ctx.closePath();
-
+            this.circle(x, y, 14, outlineColor);
 
             // knob indicator
             let fraction = valueDict[index];
             if (undefined == fraction)
-               fraction = 0.5;
+               fraction = 0.0;
 
-            this.ctx.strokeStyle = fullColor;
-            if (fraction > 0.0) {
-
-               this.ctx.beginPath();
-               this.ctx.arc(x, y, 10, zeroAngle, zeroAngle + (fraction * 2 * Math.PI));
-               this.ctx.lineWidth = 7;
-               this.ctx.stroke();
-               this.ctx.closePath();
-            }
+            if (fraction > 0.0)
+               this.ring(x, y, 10, 7, fullColor, fraction, 0.5 * Math.PI);
 
             // inner circle
-            this.ctx.fillStyle = outlineColor;
-            this.ctx.beginPath();
-            this.ctx.arc(x, y, 9, 0, 2 * Math.PI);
-            this.ctx.fill();
-            this.ctx.closePath();
+            this.circle(x, y, 9, outlineColor);
 
             // color inidicator
             let color = colorDict[index];
             if (undefined == color)
                color = "#000000";
-            this.ctx.fillStyle = color;
 
-            this.ctx.beginPath();
-            this.ctx.arc(x, y, 7, 0, 2 * Math.PI);
-            this.ctx.fill();
-            this.ctx.closePath();
+            this.circle(x, y, 7, color);
+
 
             // name 
             let name = nameDict[index];
-            this.ctx.fillStyle = fullColor;
+            let fontColor = fullColor;
             if (undefined == name) {
                name = index.toString();
-               this.ctx.fillStyle = outlineColor;
+               fontColor = outlineColor;
             }
 
+            this.ctx.fillStyle = fontColor;
             this.ctx.fillText(name, x, y + 25);
 
          }
